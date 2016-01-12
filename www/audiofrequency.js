e98b116f7431b2f10b6e38f6874bf42ce821de1d
an-rohangovekar <rohan.g@applicationnexus.com>
2016-01-12 13:10:50 +0530

    Update audiofrequency.js

--- www/audiofrequency.js
+++ www/audiofrequency.js
@@ -23,7 +23,7 @@ var AudioFrequency = function () {
 AudioFrequency.onHasSubscribersChange = function () {
   // If we just registered the first handler, make sure native listener is started.
   if (this.numHandlers === 1 && handlers() === 1) {
-     Log.v("ConsoleLog", "frequency set : "+frequency);
+    
       exec(audiofrequency._frequency, audiofrequency._error, "AudioFrequency", "start", [frequency]);
   } else if (handlers() === 0) {
       exec(null, null, "AudioFrequency", "stop", []);
